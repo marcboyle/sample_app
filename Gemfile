448e5d69658590f7b2a71f562c657b56de0c5c3f
Marc Boyle <marc.boyle@boyletransport.com>
2012-07-20 15:44:58 -0400

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -17,15 +17,18 @@ gem 'annotate', '~> 2.4.1.beta', group: :development
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '3.2.4'
-  gem 'coffee-rails', '3.2.2'
-  gem 'uglifier', 	'1.2.3'
+	gem 'sass-rails',   '3.2.4'
+	gem 'coffee-rails', '3.2.2'
+	gem 'uglifier', 	'1.2.3'
 end
 
 gem 'jquery-rails',	'2.0.0'
 
 group :test do
 	gem 'capybara',	'1.1.2'
+	gem 'factory_girl_rails', '1.4.0'
+	gem 'cucumber-rails', '1.2.1', require: false
+	gem 'database_cleaner', '0.7.0'
 end
 
 group :production do
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+
+describe Micropost do
+
+  let(:user) { FactoryGirl.create(:user) }
+  before { @micropost = user.microposts.build(content: "Lorem ipsum")}
+
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should == user }
+
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do 
+  	before { @micropost.user_id = nil }
+  	it { should_not be_valid }
+  end
+
+
+ describe "with blank content" do
+    before { @micropost.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @micropost.content = "a" * 141 }
+    it { should_not be_valid }
+  end
+
+  describe "accessible attributes" do
+    it "should not allow access to user_id" do
+      expect do
+        Micropost.new(user_id: user.id)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.should change(Micropost, :count).by(-1)
+      end
+    end
+  end
+
+
+
+end
